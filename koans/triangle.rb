776dc1c7f72f4fd157f50b36eb1a39371ad51eba
leblancr <rich.leblanc1@proton.me>
2025-02-26 01:03:39 -0500

    Finished about_triangle_project_2

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if (a == b && a != c) || (a == c && a != b) || (b == c && b != a)
+  # Check for negative or zero values
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, "Sides must be positive"
+  # Check for triangle inequality violations
+  elsif a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError, "Invalid triangle sides"
+  elsif
+    (a == b && a != c) || (a == c && a != b) || (b == c && b != a)
     puts "Two of the values are equal, and one is different."
     :isosceles
   else
